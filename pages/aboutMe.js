8b090f59539a2f97037202107f48fe585ed1196a
omarouhra <omarmaroc26@gmail.com>
2021-11-13 18:27:05 +0100

    about me page

--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -1,9 +1,30 @@
+import Footer from "../components/Footer";
+import NavBar from "../components/NavBar";
+
 function aboutMe() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <main className='max-w-screen-xl mx-auto spacing  px-4 lg:px-12'>
+        <h2 className='title'>About me</h2>
+        <p className='text sm:w-4/5 md:w-3/5'>
+          Hello! <br />
+          if you're wondering who's the person behind this work, then this
+          section is made for you! Let me just tell you that I am exactly like
+          you at this point, I am always curious about what's behind the
+          professional serious image of any project owner. The branding content
+          is always perfectly curated, and this makes a lot of sense! That's why
+          I considered this space to be just me, OMAR, your new friend!! Well..
+          I was born in Morocco in a town called Agadir. And I grew up hating
+          school 😂 . I could be wrong but I still have the same feeling! I
+          adore creating beautiful things and sense the pride and the passion to
+          share them. I'm a fan of minimalism! Next to coding and managing my
+          lovely project, I love music, calligraphy and coffee!
+        </p>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default aboutMe
+export default aboutMe;
