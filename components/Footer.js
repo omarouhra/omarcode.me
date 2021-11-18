e20dd4e4d9f51416790b47eeb03d089476f3efb0
omarouhra <omarmaroc26@gmail.com>
2021-11-18 16:45:02 +0100

    footer reset

--- components/Footer.js
+++ components/Footer.js
@@ -4,23 +4,23 @@ import NavBar from "./NavBar";
 function Footer() {
   return (
     <footer >
-      <div className='mt-12 lg:mt-24 flex items-center justify-center text-center '>
-        <div className='space-y-4'>
+      <div className='mt-12 lg:mt-24'>
+        <div className='mb-12'>
           <h2 className='title'>Now about your project !</h2>
-          <p className='text-sm font-normal   md:text-base'>
+          <p className='text-sm font-normal  my-8 md:text-base'>
             I’m currently{" "}
             <span className='text-blue-500 font-bold'>available</span>
           </p>
 
-          <p className='text-sm font-normal  md:text-base text-gray-800  leading-loose '>
+          <p className='text-sm font-normal my-8 md:text-base text-gray-800  leading-loose '>
             Looking for a developer? Let's work together and empower your
             business.
             <br /> Another option appears to be a question or a DM to connect!
           </p>
           <CustomLink title='omar@omarcode.me' important link='#' />
         </div>
-      </div>
         <NavBar />
+      </div>
     </footer>
   );
 }
