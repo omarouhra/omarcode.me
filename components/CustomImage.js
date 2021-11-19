adb6a1d44d02cc1e27b0bd6bde097006c2a9a3d1
omarouhra <omarmaroc26@gmail.com>
2021-11-19 23:46:58 +0100

    add image to about me page

--- components/CustomImage.js
+++ components/CustomImage.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-function CustomImage({ image, alt, className }) {
+function CustomImage({ image, alt, className, shadow }) {
   return <Image src={image} alt={alt} layout='fill' className={className} />;
 }
 
--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -4,6 +4,8 @@ import Head from "next/head";
 import { motion } from "framer-motion";
 import CustomLink from "../components/CustomLink";
 import { RoughNotation } from "react-rough-notation";
+import CustomImage from "../components/CustomImage";
+import HeroImage from "../public/images/aboutMe.png";
 
 function aboutMe() {
   const transition = { duration: 0.7, ease: [0.4, 0.13, 0.23, 0.9] };
@@ -30,7 +32,7 @@ function aboutMe() {
           <div className='mt-12 lg:mt-0'>
             <CustomLink link='/' title='← Go Back' />
           </div>
-          <h2 className=' mt-8 text-xl md:text-2xl lg:text-3xl font-bold'>
+          <h2 className=' my-8 text-xl md:text-2xl lg:text-3xl font-bold'>
             {" "}
             <RoughNotation
               type='highlight'
@@ -40,19 +42,32 @@ function aboutMe() {
               About Me
             </RoughNotation>
           </h2>
-          <p className=' text-sm  mt-4 leading-normal md:leading-loose  md:text-base  text-gray-700 font-normal '>
-            Hello! If you're wondering who's the person behind this work, then
-            this section is made for you! Let me just tell you that I am exactly
-            like you at this point, I am always curious about what's behind the
-            professional serious image of any project owner. The branding
-            content is always perfectly curated, and this makes a lot of sense!
-            That's why I considered this space to be just me, OMAR, your new
-            friend! Well.. I was born in Morocco in Agadir city. I grew up
-            hating school. I could be wrong but I still have the same feeling! I
-            adore creating beautiful things and sense the pride and the passion
-            to share them. I'm a fan of minimalism! Next to coding and managing
-            my small business, I love music, calligraphy and coffee!
-          </p>
+          <div className='flex flex-col space-y-12 lg:space-y-0 lg:flex-row-reverse lg:justify-between lg:items-center '>
+            <div className='lg:w-2/3'>
+              <p className=' text-sm  mt-4 leading-normal md:leading-loose  md:text-base  text-gray-700 font-normal text-justify '>
+                Hello! If you're wondering who's the person behind this work,
+                then this section is made for you! Let me just tell you that I
+                am exactly like you at this point, I am always curious about
+                what's behind the professional serious image of any project
+                owner. The branding content is always perfectly curated, and
+                this makes a lot of sense! That's why I considered this space to
+                be just me, OMAR, your new friend! Well.. I was born in Morocco
+                in Agadir city. I grew up hating school. I could be wrong but I
+                still have the same feeling! I adore creating beautiful things
+                and sense the pride and the passion to share them. I'm a fan of
+                minimalism! Next to coding and managing my small business, I
+                love music, calligraphy and coffee!
+              </p>
+            </div>
+
+            <div className='relative w-[300px] lg:w-1/3   h-[410px] mr-12  '>
+              <CustomImage
+                image={HeroImage.src}
+                alt='Hero Image'
+                className='object-contain '
+              />
+            </div>
+          </div>
         </div>
         <div className='flex flex-col space-y-6 lg:flex-row-reverse lg:justify-between lg:space-y-0 lg:items-center'>
           <div className='lg:w-1/2'>
