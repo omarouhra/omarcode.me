1ee4e013d6f01b6c27cded24abfbf0d978811f8c
Omar Ouhra <omarmaroc26@gmail.com>
2022-12-22 00:56:08 +0100

    feat: Next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import Navbar from "@/components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="h-screen w-full bg-gradient-to-r from-brand_purple_dark to-brand_purple">
-      <Component {...pageProps} />
-    </div>
+    <ThemeProvider defaultTheme="system" attribute="class">
+      <div className="h-screen w-full bg-white dark:bg-gradient-to-r  dark:from-brand_purple_dark  dark:to-brand_purple">
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "media",
   theme: {
     extend: {
       colors: {
         brand_purple_dark: "#14121A",
         brand_purple: "#33142A",
+        brand_gray: "#C5C5C5",
       },
     },
   },
